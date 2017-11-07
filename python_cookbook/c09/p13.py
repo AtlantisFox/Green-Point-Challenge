05e5dbbc541ff00ba5c131bddd86bab1166d6782
Will-Z <evilliril@gmail.com>
2017-11-07 11:15:06 +0800

    add cache instance

--- python_cookbook/c09/p13.py
+++ python_cookbook/c09/p13.py
@@ -1,3 +1,5 @@
+import weakref
+
 
 # 无实例
 class NoInstance(type):
@@ -30,6 +32,26 @@ class SingleSpam(metaclass=Singleton):
         print('Creating Spam (singleton)')
 
 
+# 缓存实例
+class Cached(type):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__cache = weakref.WeakValueDictionary()
+
+    def __call__(self, *args):
+        if args in self.__cache:
+            return self.__cache[args]
+        else:
+            obj = super().__call__(*args)
+            self.__cache[args] = obj
+            return obj
+
+
+class CacheSpam(metaclass=Cached):
+    def __init__(self, name):
+        print('Creating Spam({!r})'.format(name))
+        self.name = name
+
 # -------------
 class Test(object):
 
@@ -49,14 +71,32 @@ class Test(object):
         else:
             print('F')
 
+    # 缓存
+    @staticmethod
+    def cache():
+        a = CacheSpam('alpha')
+        b = CacheSpam('beta')
+        c = CacheSpam('alpha')
+
+        if a is b:
+            print('a is b')
+        else:
+            print('a is not b')
+
+        if a is c:
+            print('a is c')
+        else:
+            print('a is not c')
+
 
 
 if __name__ == '__main__':
 
     # Test.noinstance()
 
-    Test.singleton()
+    # Test.singleton()
 
+    Test.cache()
 
 
 
